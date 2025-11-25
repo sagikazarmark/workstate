995267f02229977f7212340d318cff257fcb82f7
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2025-11-25 02:45:30 +0100

    fix: optional dependencies
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- src/workstate/__init__.py
+++ src/workstate/__init__.py
@@ -1,9 +1,18 @@
-from . import fsspec, obstore
+from importlib.util import find_spec
+
 from .file import FileLoader, FilePersister
 
 __all__ = [
-    "fsspec",
-    "obstore",
     "FileLoader",
     "FilePersister",
 ]
+
+if find_spec("fsspec") is not None:
+    from . import fsspec  # noqa: F401
+
+    __all__.append("fsspec")
+
+if find_spec("obstore") is not None:
+    from . import obstore  # noqa: F401
+
+    __all__.append("obstore")
